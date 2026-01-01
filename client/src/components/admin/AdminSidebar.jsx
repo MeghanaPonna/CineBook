864375478f3deaffa651e0abd3ca1743b0667f55
Meghana <meghanaponna234@gmail.com>
2026-01-01 19:02:21 +0530

    updated admin sidebar with real image and name

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { assets } from '../../assets/assets'
 import { LayoutDashboardIcon, ListCollapseIcon, ListIcon, PlusSquareIcon } from 'lucide-react'
 import { NavLink } from 'react-router-dom'
+import { useUser } from "@clerk/clerk-react";
 
 
 const AdminSidebar = () => {
 
-    const user = {
-        firstName: 'Admin',
-        lastName: 'User',
-        imageUrl: assets.profile,
-    }
+    // const user = {
+    //     firstName: 'Admin',
+    //     lastName: 'User',
+    //     imageUrl: assets.profile,
+    // }
+
+    const { user } = useUser();
 
     const adminNavLinks = [
         { name: 'Dashboard', path: '/admin', icon: LayoutDashboardIcon},
@@ -21,8 +24,9 @@ const AdminSidebar = () => {
 
   return (
     <div className='h-[calc(100vh-64px)] md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-gray-300/20 text-sm'>
-        <img className='mt-2 text-base w-20 h-20 rounded-full object-cover max-md:hidden' src={user.imageUrl} alt="sidebar" />
-        <p className='mt-2 text-base max-md:hidden'>{user.firstName} {user.lastName}</p>
+        <img className='mt-2 text-base w-20 h-20 rounded-full object-cover max-md:hidden' src={user?.imageUrl} alt="sidebar" />
+        {/* <p className='mt-2 text-base max-md:hidden'>{user.firstName} {user.lastName}</p> */}
+        <p className='mt-2 text-base max-md:hidden'> {user?.fullName || "Admin User"}</p>
 
         <div className='w-full'>
             {adminNavLinks.map((link,index)=>(
