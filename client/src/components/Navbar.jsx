78a345a1d8ff3252d65fcf0581435834d2a5d250
Meghana <meghanaponna234@gmail.com>
2026-01-01 19:04:38 +0530

    'CineBook' logo sized is updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
       {/* <Link to='/' className='max-md:flex-1'> */}
       <NavLink to="/" className="max-md:flex-1">
-      <img src={assets.logo} alt="" className='w-26 h-auto' />
+      <img src={assets.logo} alt="" className='w-50 h-auto' />
       </NavLink>
 
       {/* menu items-navigate different pages */}
