284917f866087d367928bd2e16a27afc329d9ea1
Meghana <meghanaponna234@gmail.com>
2026-01-01 12:46:37 +0530

    added appContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,10 +15,15 @@ import AddShows from './pages/admin/AddShows'
 import Dashboard from './pages/admin/Dashboard'
 import ListShows from './pages/admin/ListShows'
 import ListBookings from './pages/admin/ListBookings'
+import { SignIn } from '@clerk/clerk-react'
+import { useAppContext } from './context/AppContext'
 
 const App = () => {
   // whenever the route is admin do not show navbar
   const isAdminRoute = useLocation().pathname.startsWith('/admin');
+  
+  const { user } = useAppContext()
+
   return (
     <>
     <Toaster/>
@@ -32,12 +37,12 @@ const App = () => {
           <Route path='/favorite' element={<Favorite/>} />
 
           {/* admin routes */}
-          {/* <Route path='/admin/*' element={user ? <Layout/> : (
+          {/* <Route path='/admin/*' element={<Layout/>}> */}
+          <Route path='/admin/*' element={user ? <Layout/> : (
             <div className='min-h-screen flex justify-center items-center'>
             <SignIn fallbackRedirectUrl={'/admin'}/>
             </div>
-           )}> */}
-           <Route path='/admin/*' element={<Layout/>}>
+           )}>
             <Route index element={<Dashboard/>} />
             <Route path='add-shows' element={<AddShows/>} />
             <Route path='list-shows' element={<ListShows/>} />
