0ac0d06587fbcf4ee1134d9103b21282a5e25a8b
Meghana <meghanaponna234@gmail.com>
2026-01-02 12:54:50 +0530

    booking confirmation email added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,3 +1,4 @@
+import sendEmail from "../configs/nodeMailer.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
 import User from "../models/User.js";
@@ -77,10 +78,63 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
 )
 
 
+// INNGEST  function to send email when user books a show
+const sendBookingConfirmationEmail = inngest.createFunction(
+    {id: 'send-booking-confirmation-email'},
+    {event: 'app/show.booked'},
+    async ({event, step}) => {
+        const { bookingId }= event.data;
+
+        const booking = await Booking.findById(bookingId).populate({
+            path: 'show',
+            populate: {path: 'movie', model: 'Movie'}
+        }).populate('user');
+
+        // sending email from nodemailer
+        await sendEmail({
+        to: booking.user.email,
+        subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
+        body: `
+            <div style="font-family: Arial, sans-serif; line-height: 1.5;">
+            <h2>Hi ${booking.user.name},</h2>
+
+            <p>
+                Your booking for 
+                <strong style="color: #F84565;">'${booking.show.movie.title}'</strong>
+                is confirmed.
+            </p>
+
+            <p>
+                <strong>Date:</strong> 
+                ${new Date(booking.show.showDateTime).toLocaleDateString(
+                'en-US',
+                { timeZone: 'Asia/Kolkata' }
+                )}<br/>
+
+                <strong>Time:</strong> 
+                ${new Date(booking.show.showDateTime).toLocaleTimeString(
+                'en-US',
+                { timeZone: 'Asia/Kolkata' }
+                )}
+            </p>
+
+            <p>Enjoy the show! 🍿 </p>
+
+            <p>Thanks for booking with us!<br/>— CineBook Team</p>
+            </div>
+        `
+        });
+
+                
+    } 
+)
+
+
 
 export const functions = [
     syncUserCreation, 
     syncUserDeletion,
     syncUserUpdate,
     releaseSeatsAndDeleteBooking,
+    sendBookingConfirmationEmail,
 ];
